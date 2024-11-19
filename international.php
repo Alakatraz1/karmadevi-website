d77c6ba54bc2d9f7a95a4163a963090dd89f027a
Sumit Dubey <sumitkumardubey1180@gmail.com>
2024-11-19 12:14:17 +0530

    Breadcrumb area fixed international page

--- international.php
+++ international.php
@@ -64,34 +64,27 @@
    <main>
 
       <!-- course details breadcrumb start -->
-      <section class="tp-breadcrumb__area pt-25 pb-55 p-relative z-index-1 fix">
-         <div class="tp-breadcrumb__bg" data-background="assets/img/breadcrumb/breadcrumb-bg-2.jpg"></div>
-         <div class="container">
-            <div class="row align-items-center">
-               <div class="col-sm-12">
-                  <div class="tp-breadcrumb__content">
-                     <div class="tp-breadcrumb__list course-details mb-70">
-                        <span><a href="index.php"><svg width="17" height="14" viewBox="0 0 17 14" fill="none"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                 <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M8.07207 0C8.19331 0 8.31107 0.0404348 8.40664 0.114882L16.1539 6.14233L15.4847 6.98713L14.5385 6.25079V12.8994C14.538 13.1843 14.4243 13.4574 14.2225 13.6589C14.0206 13.8604 13.747 13.9738 13.4616 13.9743H2.69231C2.40688 13.9737 2.13329 13.8603 1.93146 13.6588C1.72962 13.4573 1.61597 13.1843 1.61539 12.8994V6.2459L0.669148 6.98235L0 6.1376L7.7375 0.114882C7.83308 0.0404348 7.95083 0 8.07207 0ZM8.07694 1.22084L2.69231 5.40777V12.8994H13.4616V5.41341L8.07694 1.22084Z"
-                                    fill="currentColor"></path>
-                              </svg></a></span>
-                        <span>International Collaboration</span>
-                     </div>
-
-                     <div class="tp-course-details-2-header">
-                        <span class="tp-course-details-2-category">K.D. Group</span>
-                        <h3 class="tp-course-details-2-title" style=text-align:center;font-size:50px>International
-                           <br>Collaborations
-                        </h3>
-
-                     </div>
+      <section class="tp-breadcrumb__area pt-90 pb-90 p-relative z-index-1 fix">
+      <div class="tp-breadcrumb__bg" data-background="assets/gd/breadcrumb/blog-stories-bg.png"></div>
+      <div class="container">
+         <div class="row align-items-center">
+            <div class="col-sm-12">
+               <div class="tp-breadcrumb__content">
+                  <div class="tp-breadcrumb__list inner-after">
+                     <span class="white"><a href="index.php"><svg width="17" height="14" viewBox="0 0 17 14"
+                              fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd"
+                                 d="M8.07207 0C8.19331 0 8.31107 0.0404348 8.40664 0.114882L16.1539 6.14233L15.4847 6.98713L14.5385 6.25079V12.8994C14.538 13.1843 14.4243 13.4574 14.2225 13.6589C14.0206 13.8604 13.747 13.9738 13.4616 13.9743H2.69231C2.40688 13.9737 2.13329 13.8603 1.93146 13.6588C1.72962 13.4573 1.61597 13.1843 1.61539 12.8994V6.2459L0.669148 6.98235L0 6.1376L7.7375 0.114882C7.83308 0.0404348 7.95083 0 8.07207 0ZM8.07694 1.22084L2.69231 5.40777V12.8994H13.4616V5.41341L8.07694 1.22084Z"
+                                 fill="currentColor" />
+                           </svg></a></span>
+                     <span class="white">Contact</span>
                   </div>
+                  <h3 class="tp-breadcrumb__title color">Contact</h3>
                </div>
             </div>
          </div>
-      </section>
+      </div>
+   </section>
       <!-- course details breadcrumb end -->
 
       <style>
