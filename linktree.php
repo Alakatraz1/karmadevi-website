57355ad4507b3b28d8d12b545c68d4bca52b9a24
Shalini Mishra <shalinimishraji2002@gmail.com>
2024-12-05 15:22:46 +0530

    add link of the social media

--- linktree.php
+++ linktree.php
@@ -1,145 +1,162 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta charset="utf-8">
-   <meta http-equiv="x-ua-compatible" content="ie=edge">
-   <title>Karma Devi Group</title>
-   <meta name="description" content="">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Karma Devi Group</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-   <!-- Place favicon.ico in the root directory -->
-   <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
 
-   <!-- CSS here -->
-   <link rel="stylesheet" href="assets/css/bootstrap.css">
-   <link rel="stylesheet" href="assets/css/animate.css">
-   <link rel="stylesheet" href="assets/css/swiper-bundle.css">
-   <link rel="stylesheet" href="assets/css/slick.css">
-   <link rel="stylesheet" href="assets/css/magnific-popup.css">
-   <link rel="stylesheet" href="assets/css/flatpickr.min.css">
-   <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
-   <link rel="stylesheet" href="assets/css/spacing.css">
-   <link rel="stylesheet" href="assets/css/main.css">
-        <link href="assets/css/styles.css" rel="stylesheet">
-        <link href="assets/css/styles2.css" rel="stylesheet">
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
-        <link rel="shortcut icon" href="assets/images/favicon.png" type="image/webp">
-        <link rel="apple-touch-icon" sizes="180x180" href="images/favicon.png">
-        <link rel="icon" href="assets/images/favicon.png" type="image/webp">
-        <title>Linktree template</title>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
-    </head>
-    <body>
-        <style>
-            .tp-header-transparent {
-    position: absolute;
-    top: 0px;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    z-index: 99;
-}
-        </style>
-	<?php include('includes/navbarhome.php')?>
-    
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/flatpickr.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
+    <link rel="stylesheet" href="assets/css/spacing.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link href="assets/css/styles.css" rel="stylesheet">
+    <link href="assets/css/styles2.css" rel="stylesheet">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/webp">
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon.png">
+    <link rel="icon" href="assets/images/favicon.png" type="image/webp">
+    <title>Linktree template</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;display=swap" rel="stylesheet">
+</head>
 
-        <!-- Snowfall Background Animation -->
-        <section class="animated-background">
-            <div id="stars1"></div>
-            <div id="stars2"></div>
-            <div id="stars3"></div>
-        </section>
-        <!-- End of Snowfall Background Animation -->
+<body>
+    <style>
+    .tp-header-transparent {
+        position: absolute;
+        top: 0px;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        z-index: 99;
+    }
+    </style>
+    <?php include('includes/navbarhome.php')?>
 
-        <div class="min-h-full flex-h-center" id="background_div">
-            <input type="hidden" value="https://bio.link" id="app-url">
-                <input type="hidden" value="null" id="is-featured">
-                    <canvas id="bg-canvas" class="background-overlay"></canvas>
-                </input>
-            </input>
 
-            <div class="mt-48 pt-120 page-full-wrap relative ">
-                <input type="hidden" value="creator-page" id="page-type">
-                <img class="display-image m-auto" data-src="assets/images/logo.png" src="assets/images/logo.png" alt="[Your photo alt]"/>
-                <h2 class="page-title page-text-color page-text-font mt-16 text-center">Your name</h2>
-                       
-                <div class="mt-24">
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261652" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/linkedin.png" src="assets/images/linkedin.png" alt="LinkedIn @ [User]"/>
-                            <span class=" item-title text-center">LinkedIn @ [User]</span>
-                        </a>
-                    </div>        
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/github.png" src="assets/images/github.png" alt="GitHub @ [User]"/>
-                            <span class=" item-title text-center">GitHub @ [User]</span>
-                        </a>
-                    </div>    
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/credly.png" src="assets/images/credly.png" alt="Credly @ [User]"/>
-                            <span class=" item-title text-center">Credly @ [User]</span>
-                        </a>
-                    </div>                            
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/hackerrank.png" src="assets/images/hackerrank.png" alt="HackerRank @ [User]"/>
-                            <span class=" item-title text-center">HackerRank @ [User]</span>
-                        </a>
-                    </div>
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/replit.png" src="assets/images/replit.png" alt="Replit @ [User]"/>
-                            <span class=" item-title text-center">Replit @ [User]</span>
-                        </a>
-                    </div>
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/instagram.png" src="assets/images/instagram.png" alt="Instagram @ [User]"/>
-                            <span class=" item-title text-center">Instagram @ [User]</span>
-                        </a>
-                    </div>
-                    <div class="page-item-wrap relative">
-                        <div class="page-item flex-both-center absolute"></div>
-                        <a target="_blank"  class="page-item-each py-10 flex-both-center" href="[Link]" data-id="261685" data-type="page_item">
-                            <img class="link-each-image" data-src="assets/images/facebook.png" src="assets/images/facebook.png" alt="Facebook @ [User]"/>
-                            <span class=" item-title text-center">Facebook @ [User]</span>
-                        </a>
-                    </div>                    
-                </div>                                                                                
+    <!-- Snowfall Background Animation -->
+    <section class="animated-background">
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+    </section>
+    <!-- End of Snowfall Background Animation -->
+
+    <div class="min-h-full flex-h-center" id="background_div">
+        <input type="hidden" value="https://bio.link" id="app-url">
+        <input type="hidden" value="null" id="is-featured">
+        <canvas id="bg-canvas" class="background-overlay"></canvas>
+        </input>
+        </input>
+
+        <div class="mt-48 pt-120 page-full-wrap relative ">
+            <input type="hidden" value="creator-page" id="page-type">
+            <img class="display-image m-auto" data-src="assets/images/logo.png" src="assets/images/logo.png"
+                alt="[Your photo alt]" />
+            <h2 class="page-title page-text-color page-text-font mt-16 text-center">Your name</h2>
+
+            <div class="mt-24">
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://www.linkedin.com"
+                        data-id="261652" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/linkedin.png"
+                            src="assets/images/linkedin.png" alt="LinkedIn @ [User]" />
+                        <span class=" item-title text-center">LinkedIn @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://github.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/github.png" src="assets/images/github.png"
+                            alt="GitHub @ [User]" />
+                        <span class=" item-title text-center">GitHub @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://info.credly.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/credly.png" src="assets/images/credly.png"
+                            alt="Credly @ [User]" />
+                        <span class=" item-title text-center">Credly @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://www.hackerrank.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/hackerrank.png"
+                            src="assets/images/hackerrank.png" alt="HackerRank @ [User]" />
+                        <span class=" item-title text-center">HackerRank @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://replit.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/replit.png" src="assets/images/replit.png"
+                            alt="Replit @ [User]" />
+                        <span class=" item-title text-center">Replit @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://www.instagram.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/instagram.png"
+                            src="assets/images/instagram.png" alt="Instagram @ [User]" />
+                        <span class=" item-title text-center">Instagram @ [User]</span>
+                    </a>
+                </div>
+                <div class="page-item-wrap relative">
+                    <div class="page-item flex-both-center absolute"></div>
+                    <a target="_blank" class="page-item-each py-10 flex-both-center" href="https://www.facebook.com"
+                        data-id="261685" data-type="page_item">
+                        <img class="link-each-image" data-src="assets/images/facebook.png"
+                            src="assets/images/facebook.png" alt="Facebook @ [User]" />
+                        <span class=" item-title text-center">Facebook @ [User]</span>
+                    </a>
+                </div>
             </div>
         </div>
-		<?php include('includes/footer.php')?>
-        
-   <script src="assets/js/vendor/jquery.js"></script>
-   <script src="assets/js/vendor/waypoints.js"></script>
-   <script src="assets/js/bootstrap-bundle.js"></script>
-   <script src="assets/js/swiper-bundle.js"></script>
-   <script src="assets/js/slick.js"></script>
-   <script src="assets/js/magnific-popup.js"></script>
-   <script src="assets/js/nice-select.js"></script>
-   <script src="assets/js/purecounter.js"></script>
-   <script src="assets/js/wow.js"></script>
-   <script src="assets/js/isotope-pkgd.js"></script>
-   <script src="assets/js/hover-reveal.js"></script>
-   <script src="assets/js/tween-max.js"></script>
-   <script src="assets/js/imagesloaded-pkgd.js"></script>
-   <script src="assets/js/flatpickr.js"></script>
-   <script src="assets/js/ajax-form.js"></script>
-   <script src="assets/js/main.js"></script>
-    </body>
+    </div>
+    <?php include('includes/footer.php')?>
 
-    <!--
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/waypoints.js"></script>
+    <script src="assets/js/bootstrap-bundle.js"></script>
+    <script src="assets/js/swiper-bundle.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/magnific-popup.js"></script>
+    <script src="assets/js/nice-select.js"></script>
+    <script src="assets/js/purecounter.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/isotope-pkgd.js"></script>
+    <script src="assets/js/hover-reveal.js"></script>
+    <script src="assets/js/tween-max.js"></script>
+    <script src="assets/js/imagesloaded-pkgd.js"></script>
+    <script src="assets/js/flatpickr.js"></script>
+    <script src="assets/js/ajax-form.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+<!--
     A different snowfall    
     <script  src="assets/js/snowfall.js"></script>
     -->
+
 </html>
